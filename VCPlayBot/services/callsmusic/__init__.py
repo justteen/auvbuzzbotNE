86012c500298f8f6a6f06e88e5c54055ee1f9912
justteen <48372964+justteen@users.noreply.github.com>
2021-07-13 03:04:38 +0700

    Update __init__.py

--- VCPlayBot/services/callsmusic/__init__.py
+++ VCPlayBot/services/callsmusic/__init__.py
@@ -1,2 +1,6 @@
-from VCPlayBot.services.callsmusic import queues
-from VCPlayBot.services.callsmusic.callsmusic import pytgcalls, run
+from pyrogram import Client
+
+from VCPlayBot import config
+
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+run = client.run

61ca994077b3a705d2a4a2a8e5ddd24d0e04f64a
justteen <48372964+justteen@users.noreply.github.com>
2021-07-13 00:24:31 +0700

    Update callsmusic.py

--- VCPlayBot/services/callsmusic/callsmusic.py
+++ VCPlayBot/services/callsmusic/callsmusic.py
@@ -1,8 +1,28 @@
 from typing import Dict
 from pytgcalls import GroupCall
+from pyrogram import Client
+from pytgcalls import PyTgCalls
 
+from VCPlayBot.config import API_HASH, API_ID, SESSION_NAME
+from VCPlayBot.services.callsmusic import queues
 from VCPlayBot.services.callsmusic import client
 from VCPlayBot.services.queues import queues
+
+client = Client(SESSION_NAME, API_ID, API_HASH)
+pytgcalls = PyTgCalls(client)
+
+
+@pytgcalls.on_stream_end()
+def on_stream_end(chat_id: int) -> None:
+    queues.task_done(chat_id)
+
+    if queues.is_empty(chat_id):
+        pytgcalls.leave_group_call(chat_id)
+    else:
+        pytgcalls.change_stream(chat_id, queues.get(chat_id)["file"])
+
+
+run = pytgcalls.run
 
 instances: Dict[int, GroupCall] = {}
 active_chats: Dict[int, Dict[str, bool]] = {}
